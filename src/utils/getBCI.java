3a9e36dc771b238a8d484039d1bf5f94e21f238a
Dheeraj <dhirajyadav135@gmail.com>
2020-10-25 20:34:00 +0530

    Add some comments.

--- src/utils/getBCI.java
+++ src/utils/getBCI.java
@@ -11,6 +11,16 @@ import soot.tagkit.Tag;
 public class getBCI {
 	public static int get(Unit u) {
 		int _ret = -1;
+
+		/*
+			We are splitting the unit into "boxes" and for semi-boxes we check if any one of 
+			them is JNewArray ot JNewMultiArray, then return BCI of the expression.
+
+			I don't know if this will always work. It is based on the data I got by printing
+			a lot of stuff. 
+
+			Do We need to include JNew in this?
+		*/
 		for(ValueBox ub: u.getUseBoxes() ) {
 			if(!ub.getClass().toString().equals("class soot.jimple.internal.JAssignStmt$LinkedRValueBox"))
                 continue;
@@ -20,6 +30,7 @@ public class getBCI {
             if(v instanceof JNewArrayExpr || v instanceof JNewMultiArrayExpr )
                 ;
 			else continue;
+
 			BytecodeOffsetTag tg = (BytecodeOffsetTag) ub.getTag("BytecodeOffsetTag");
 			if(tg != null)
 				return tg.getBytecodeOffset();
