2e5ed4174852e9838f9859d21105572204999bd3
Dheeraj <dhirajyadav135@gmail.com>
2020-10-25 19:27:57 +0530

    Modify BCI value for JNewArray and JNewMultiArray

--- src/utils/getBCI.java
+++ src/utils/getBCI.java
@@ -1,12 +1,30 @@
 package utils;
 
 import soot.Unit;
+import soot.Value;
+import soot.ValueBox;
+import soot.jimple.internal.JNewArrayExpr;
+import soot.jimple.internal.JNewMultiArrayExpr;
 import soot.tagkit.BytecodeOffsetTag;
 import soot.tagkit.Tag;
 
 public class getBCI {
 	public static int get(Unit u) {
 		int _ret = -1;
+		for(ValueBox ub: u.getUseBoxes() ) {
+			if(!ub.getClass().toString().equals("class soot.jimple.internal.JAssignStmt$LinkedRValueBox"))
+                continue;
+            Value v = ub.getValue();
+            if(v==null)
+                continue;
+            if(v instanceof JNewArrayExpr || v instanceof JNewMultiArrayExpr )
+                ;
+			else continue;
+			BytecodeOffsetTag tg = (BytecodeOffsetTag) ub.getTag("BytecodeOffsetTag");
+			if(tg != null)
+				return tg.getBytecodeOffset();
+		}
+		
 		Tag t = u.getTag("BytecodeOffsetTag");
 		if (t == null) {
 			String error = u.toString() + " doesn't have bytecodeoffset!";
