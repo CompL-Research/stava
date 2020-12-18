8779450058cc586edda6cacb3791061a6cc00f7a
Dheeraj <dhirajyadav135@gmail.com>
2020-12-18 16:16:58 +0530

    Add test7 and test8

---
+++ tests/test8/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+	public static void main(String[] args) {
+		test();
+	}
+
+	public static Node test() {
+		Node A = new Node();
+		Node B = new Node();
+		Node C = new Node();
+		Node D = new Node();
+		func(A,B);
+		return A;
+	}
+
+	public static Node func(Node p1, Node p2) {
+		p1.n = p2;
+		if(p2 != null) {
+			p2 = bar();
+		}
+		return p1;
+	}
+
+	public static Node bar() {
+		return new Node();
+	}
+}
